7e46f0df1ac319cbebf605095390695f1dfa479a
Harshk0786 <91257903+Harshk0786@users.noreply.github.com>
2023-10-17 23:04:23 +0530

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,13 @@
+alert("Welcome To InverseMovies.in");
+
+let name = prompt("What is your Name?","Guest");
+console.log(name);
+
+
+let Age = confirm("Are you above 18?");
+if(Age){
+    console.log("You can Enter to the website");
+}
+else{
+    console.log("You can not Enter to the website");
+}
